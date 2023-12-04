0b1e2221e0e532391dbc280b34caec14591ea460
Andreea's MacBook <andreeasmacbook@Andreeass-MacBook.local>
2023-12-04 17:37:56 +0200

    creare formular

--- src/app/admin-page/admin-page.component.ts
+++ src/app/admin-page/admin-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppTopBarAdminComponent} from '../app-top-bar-admin/app-top-bar-admin.component';
 import { PortofoliuAdminComponent} from '../portofoliu-admin/portofoliu-admin.component';
+import {FormComponentComponent} from '../form-component/form-component.component';
 
 @Component({
   standalone: true,
   selector: 'app-admin-page',
-  imports: [CommonModule, AppTopBarAdminComponent, PortofoliuAdminComponent],
+  imports: [CommonModule, AppTopBarAdminComponent, PortofoliuAdminComponent, FormComponentComponent],
   templateUrl: './admin-page.component.html',
   styleUrl: './admin-page.component.css'
 })
