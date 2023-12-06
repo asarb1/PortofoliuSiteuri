90927f7e7f93e04966024b7ff17a7867290afde1
Andreea's MacBook <andreeasmacbook@Andreeass-MacBook.local>
2023-12-06 23:03:40 +0200

    formulare

--- src/app/form-component/form-component.component.ts
+++ src/app/form-component/form-component.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import { Portofoliu } from 'src/app/models/tutorial.model';
+import { PortofoliuService } from 'src/app/services/tutorial.service';
 @Component({
   selector: 'app-form-component',
   standalone: true,
@@ -7,6 +9,47 @@ import {RouterModule, Routes} from '@angular/router';
   templateUrl: './form-component.component.html',
   styleUrl: './form-component.component.css'
 })
-export class FormComponentComponent {
+export class FormComponentComponent implements OnInit{
+  portofoliu: Portofoliu = {
+    imagine: '',
+    titlu: '',
+    descriere: '',
+    link: '',
+      published: false
+    };
+    submitted = false;
 
+    constructor(private portofoliuService: PortofoliuService) { }
+
+    ngOnInit(): void {
+    }
+
+    savePortofoliu(): void {
+      const data = {
+        imagine: this.portofoliu.imagine,
+        titlu: this.portofoliu.titlu,
+        descriere: this.portofoliu.descriere,
+        link: this.portofoliu.link,
+      };
+
+      this.portofoliuService.create(data)
+        .subscribe({
+          next: (res) => {
+            console.log(res);
+            this.submitted = true;
+          },
+          error: (e) => console.error(e)
+        });
+    }
+
+    newPortofoliu(): void {
+      this.submitted = false;
+      this.portofoliu = {
+        imagine: '',
+        titlu: '',
+        descriere: '',
+        link: '',
+        published: false
+      };
+    }
 }
